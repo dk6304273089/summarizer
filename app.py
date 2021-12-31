9268c3812077779911750b00d36391265264283a
unknown <dk6304273089@gmail.com>
2021-12-31 11:13:11 +0530

    update app.py file

--- app.py
+++ app.py
@@ -1,34 +1,37 @@
 import streamlit as st
-from googletrans import Translator
-from transformers import PegasusTokenizer,PegasusForConditionalGeneration
-from deep_translator import GoogleTranslator
+from transformers import PegasusTokenizer
 import torch
 import pandas as pd
+from googletrans import Translator
+translator = Translator(service_urls=[
+      'translate.google.com',
+      'translate.google.co.kr',
+    ])
 LANGUAGES = {
-    'afrikaans':'af','albanian':'sq','amharic': 'am','arabic':'ar' ,'armenian':'hy','azerbaijani':'az','basque':'eu','belarusian':'be','bengali':'bn',
-    'bosnian':'bs','bulgarian':'bg','catalan':'ca','cebuano':'ceb','chichewa':'ny','chinese (simplified)':'zh-cn','chinese (traditional)':'zh-tw','corsican':'co','croatian':'hr',
-    'czech':'cs','danish':'da','dutch':'nl','english':'en','esperanto':'eo','estonian':'et','filipino':'tl','finnish':'fi',
-    'french':'fr','frisian':'fy','galician':'gl','georgian':'ka','german':'de','greek':'el','gujarati':'gu','haitian creole':'ht',
-    'hausa':'ha','hawaiian':'haw','hebrew':'he','hindi':'hi','hmong':'hmn','hungarian':'hu','icelandic':'is',
-    'igbo':'ig','indonesian':'id','irish':'ga','italian':'it','japanese':'ja','javanese':'jw','kannada':'kn','kazakh':'kk','khmer':'km',
-    'korean':'ko','kurdish (kurmanji)':'ku','kyrgyz':'ky','lao':'lo','latin':'la','latvian':'lv','lithuanian':'lt','luxembourgish':'lb',
-    'macedonian':'mk','malagasy':'mg','malay':'ms','malayalam':'ml','maltese':'mt','maori':'mi','marathi':'mr','mongolian':'mn',
-    'myanmar (burmese)':'my','nepali':'ne','norwegian':'no','odia':'or','pashto':'ps','persian':'fa','polish':'pl','portuguese':'pt',
-    'punjabi':'pa','romanian':'ro','russian':'ru','samoan':'sm','scots gaelic':'gd','serbian':'sr','sesotho':'st','shona':'sn','sindhi':'sd',
-    'sinhala':'si','slovak':'sk','slovenian':'sl','somali':'so','spanish':'es','sundanese':'su','swahili':'sw', 'swedish':'sv','tajik':'tg',
-    'tamil':'ta','telugu':'te','thai':'th','turkish':'tr','ukrainian':'uk','urdu':'ur','uyghur':'ug','uzbek':'uz','vietnamese':'vi','welsh':'cy',
-    'xhosa':'xh','yiddish':'yi','yoruba':'yo','zulu':'zu'}
+    'Afrikaans':'af','Albanian':'sq','Amharic': 'am','Arabic':'ar' ,'Armenian':'hy','Azerbaijani':'az','Basque':'eu','Belarusian':'be','Bengali':'bn',
+    'Bosnian':'bs','Bulgarian':'bg','Catalan':'ca','Cebuano':'ceb','Chichewa':'ny','Chinese (simplified)':'zh-cn','Chinese (traditional)':'zh-tw','Corsican':'co','Croatian':'hr',
+    'Czech':'cs','Danish':'da','Dutch':'nl','English':'en','Esperanto':'eo','Estonian':'et','Filipino':'tl','Finnish':'fi',
+    'French':'fr','Frisian':'fy','Galician':'gl','Georgian':'ka','German':'de','Greek':'el','Gujarati':'gu','Haitian creole':'ht',
+    'Hausa':'ha','Hawaiian':'haw','Hebrew':'he','Hindi':'hi','Hmong':'hmn','Hungarian':'hu','Icelandic':'is',
+    'Igbo':'ig','Indonesian':'id','Irish':'ga','Italian':'it','Japanese':'ja','Javanese':'jw','Kannada':'kn','Kazakh':'kk','Khmer':'km',
+    'Korean':'ko','Kurdish (kurmanji)':'ku','Kyrgyz':'ky','Lao':'lo','Latin':'la','Latvian':'lv','Lithuanian':'lt','Luxembourgish':'lb',
+    'Macedonian':'mk','Malagasy':'mg','Malay':'ms','Malayalam':'ml','Maltese':'mt','Maori':'mi','Marathi':'mr','Mongolian':'mn',
+    'Myanmar (burmese)':'my','Nepali':'ne','Norwegian':'no','Odia':'or','Pashto':'ps','Persian':'fa','Polish':'pl','Portuguese':'pt',
+    'Punjabi':'pa','Romanian':'ro','Russian':'ru','Samoan':'sm','Scots gaelic':'gd','Serbian':'sr','Sesotho':'st','Shona':'sn','Sindhi':'sd',
+    'Sinhala':'si','Slovak':'sk','Slovenian':'sl','Somali':'so','Spanish':'es','Sundanese':'su','Swahili':'sw', 'Swedish':'sv','Tajik':'tg',
+    'Tamil':'ta','Telugu':'te','Thai':'th','Turkish':'tr','Ukrainian':'uk','Urdu':'ur','Uyghur':'ug','Uzbek':'uz','Vietnamese':'vi','Welsh':'cy',
+    'Xhosa':'xh','Yiddish':'yi','Yoruba':'yo','Zulu':'zu'}
 h=st.sidebar.selectbox("Select Activity",["Translator and Summarizer","Pegasus Tokenizer"])
 try:
     if h=="Translator and Summarizer" :
-        st.title("Language Translator and Summarizer")
+        st.subheader("LANGUAGE TRANSLATOR AND SUMMARZIER")
         col1,col2=st.columns(2)
-        input1=col1.selectbox("Source Language",['auto','afrikaans','albanian','amharic','arabic','armenian','azerbaijani','basque','belarusian','bengali','bosnian','bulgarian','catalan', 'cebuano','chichewa','chinese (simplified)','chinese (traditional)','corsican',
-    'croatian','czech','danish','dutch','english','esperanto','estonian','filipino', 'finnish', 'french','frisian','galician','georgian', 'german','greek','gujarati','haitian creole',
-    'hausa','hawaiian','hebrew', 'hebrew', 'hindi','hmong','hungarian','icelandic','igbo','indonesian','irish','italian','japanese','javanese','kannada','kazakh','khmer','korean',
-    'kurdish (kurmanji)','kyrgyz','lao','latin','latvian', 'lithuanian','luxembourgish','macedonian','malagasy','malay','malayalam','maltese','maori','marathi','mongolian','myanmar (burmese)','nepali','norwegian',
-    'odia','pashto','persian','polish','portuguese','punjabi','romanian','russian','samoan','scots gaelic', 'serbian','sesotho','shona','sindhi','sinhala','slovak','slovenian', 'somali','spanish','sundanese', 'swahili',   'swedish',  'tajik',
-    'tamil',  'telugu','thai', 'turkish','ukrainian', 'urdu', 'uyghur', 'uzbek', 'vietnamese',  'welsh','xhosa', 'yiddish', 'yoruba', 'zulu'])
+        input1=col1.selectbox("Source Language",['Auto','Afrikaans','Albanian','Amharic','Arabic','Armenian','Azerbaijani','Aasque','Belarusian','Bengali','Bosnian','Bulgarian','Catalan', 'Cebuano','Chichewa','Chinese (simplified)','Chinese (traditional)','Corsican',
+    'Croatian','Czech','Danish','Dutch','English','Esperanto','Estonian','Filipino', 'Finnish', 'French','Frisian','Galician','Georgian', 'German','Greek','Gujarati','Haitian creole',
+    'Hausa','Hawaiian','Hebrew', 'Hebrew', 'Hindi','Hmong','Hungarian','Icelandic','Igbo','Indonesian','Irish','Italian','Japanese','Javanese','Kannada','Kazakh','Khmer','Korean',
+    'Kurdish (kurmanji)','Kyrgyz','Lao','Latin','Latvian', 'Lithuanian','Luxembourgish','Macedonian','Malagasy','Malay','Malayalam','Maltese','Maori','Marathi','Mongolian','Myanmar (burmese)','Nepali','Norwegian',
+    'Odia','Pashto','Persian','Polish','Portuguese','Punjabi','Romanian','Russian','Samoan','Scots gaelic', 'Serbian','Sesotho','Shona','Sindhi','Sinhala','Slovak','Slovenian', 'Somali','Spanish','Sundanese', 'Swahili',   'Swedish',  'Tajik',
+    'Tamil',  'Telugu','Thai', 'Turkish','Ukrainian', 'Urdu', 'Uyghur', 'Uzbek', 'Vietnamese',  'Welsh','Xhosa', 'Yiddish', 'Yoruba', 'Zulu'])
         input2=col2.selectbox("Destination Language",["English"])
         col3,col4=st.columns(2)
         input3=col3.selectbox("Translator",["Yes","No"])
@@ -37,48 +40,89 @@ try:
         input6=st.button('Convert')
         if input6:
             if len(input5)>100:
-                if input3=="Yes" and input4=="No" and input1=="auto":
-                    translated = GoogleTranslator(source="auto", target="en")
-                    f = translated.translate(input5)
-                    print(f)
-                    st.success(f)
-                elif input3=="Yes" and input4=="Yes" and input1!="auto":
-                    language=LANGUAGES[input1]
-                    path = "./model/lst.pth"
-                    col3, col4 = st.columns(2)
-                    translated = GoogleTranslator(source=input1, target="en")
-                    f = translated.translate(input5)
-                    print(f)
-                    tokenizer = PegasusTokenizer.from_pretrained("./model/")
-                    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-                    model = torch.load(path)
-                    batch = tokenizer(f, truncation=True, padding='longest', return_tensors="pt").to(device)
-                    translated = model.generate(**batch)
-                    tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-                    col3_expander = col3.expander("Expand Translated Text")
-                    with col3_expander:
-                        col3_expander.write(f)
-                    col4_expander = col4.expander("Expand Summarized Text")
-                    with col4_expander:
-                        col4_expander.write(tgt_text[0])
-                elif input3=="No" and input4=="Yes" and input1=="auto" or input1!="auto":
-                    path = "./model/lst.pth"
-                    translated = GoogleTranslator(source="auto", target="en")
-                    f = translated.translate(input5)
-                    print(f)
-                    tokenizer = PegasusTokenizer.from_pretrained("./model/")
-                    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-                    model = torch.load(path)
-                    batch = tokenizer(f, truncation=True, padding='longest', return_tensors="pt").to(device)
-                    translated = model.generate(**batch)
-                    tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-                    st.success(tgt_text[0])
-                elif input3=="No" and input4=="No" and input1=="auto" or input1!="auto":
-                    st.success("No Translator and summarizer is selected")
+                try:
+                    if input3=="Yes" and input4=="No":
+                        if input1=="Auto":
+                            translated=translator.translate(input5, dest='en')
+                            f=translated.text
+                            st.success(f)
+                        else:
+                            language=LANGUAGES[input1]
+                            translated = translator.translate(input5,src=language,dest='en')
+                            f=translated.text
+                            st.success(f)
+
+                    elif input3=="Yes" and input4=="Yes":
+                        if input1=="Auto":
+                            path = "./model/lst.pth"
+                            col3, col4 = st.columns(2)
+                            translated = translator.translate(input5, dest='en')
+                            f=translated.text
+                            tokenizer = PegasusTokenizer.from_pretrained("./model/")
+                            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+                            model = torch.load(path)
+                            batch = tokenizer(f, truncation=True, padding='longest', return_tensors="pt").to(device)
+                            translated = model.generate(**batch)
+                            tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+                            col3_expander = col3.expander("Expand Translated Text")
+                            with col3_expander:
+                                col3_expander.write(f)
+                            col4_expander = col4.expander("Expand Summarized Text")
+                            with col4_expander:
+                                col4_expander.write(tgt_text[0])
+                        else:
+                            language = LANGUAGES[input1]
+                            path = "./model/lst.pth"
+                            col3, col4 = st.columns(2)
+                            translated = translator.translate(input5,src=language,dest='en')
+                            f = translated.text
+                            tokenizer = PegasusTokenizer.from_pretrained("./model/")
+                            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+                            model = torch.load(path)
+                            batch = tokenizer(f, truncation=True, padding='longest', return_tensors="pt").to(device)
+                            translated = model.generate(**batch)
+                            tgt_text = tokenizer.batch_decode(f, skip_special_tokens=True)
+                            col3_expander = col3.expander("Expand Translated Text")
+                            with col3_expander:
+                                col3_expander.write(f)
+                            col4_expander = col4.expander("Expand Summarized Text")
+                            with col4_expander:
+                                col4_expander.write(tgt_text[0])
+
+                    elif input3=="No" and input4=="Yes" and input1=="Auto" or input1!="Auto":
+                        if input1=="Auto":
+                            path = "./model/lst.pth"
+                            translated = translator.translate(input5, dest='en')
+                            f=translated.text
+                            tokenizer = PegasusTokenizer.from_pretrained("./model/")
+                            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+                            model = torch.load(path)
+                            batch = tokenizer(f, truncation=True, padding='longest', return_tensors="pt").to(device)
+                            translated = model.generate(**batch)
+                            tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+                            st.success(tgt_text[0])
+                        else:
+                            language = LANGUAGES[input1]
+                            path = "./model/lst.pth"
+                            translated = translator.translate(input5,src=language,dest='en')
+                            f=translated.text
+                            tokenizer = PegasusTokenizer.from_pretrained("./model/")
+                            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+                            model = torch.load(path)
+                            batch = tokenizer(f, truncation=True, padding='longest',
+                                              return_tensors="pt").to(device)
+                            translated = model.generate(**batch)
+                            tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+                            st.success(tgt_text[0])
+
+                    elif input3=="No" and input4=="No" and input1=="Auto" or input1!="Auto":
+                        st.success("No Translator and summarizer is selected")
+                except ValueError:
+                    st.success("You have entered an incorrect language syntax")
             else:
                 st.success("No of words in the sentence should be greater than 100")
     elif h== "Pegasus Tokenizer":
-        st.title("Pegasus Tokenizer")
+        st.subheader("PEGASUS TOKENIZER")
         user_input=st.text_area("Enter the Text")
         button1 = st.button("Predict")
         if button1:
